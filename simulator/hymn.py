0985d23aa9504ac8b3d88debf2d928d3d09f94e0
hazhou <zhouyh1997@gmail.com>
2018-10-12 15:33:38 -0400

    fixed io

--- simulator/hymn.py
+++ simulator/hymn.py
@@ -18,6 +18,7 @@ class Simulator:
         self._done = False
         self._zero_flag = True
         self._positive_flag = False
+        self._verbose = 0
         self.INSTRUCTIONS = {1: self._exec_jump,
                              2: self._exec_jzer,  
                              3: self._exec_jpos,
@@ -27,57 +28,79 @@ class Simulator:
                              7: self._exec_sub}
 
     def run(self, verbose=0):
+        self._verbose = verbose
         while not self._done and self._pc < 30:
-            self.step(verbose)
+            self.step()
         print("io: ", self._io)
         return self._io
 
-    def step(self, verbose, jump=False):
+    def step(self, jump=False):
         self._ir = self._memory[self._pc]
         instrn = np.right_shift(self._memory[self._pc], 5) & 7
         num = np.int8(self._memory[self._pc] - (instrn << 5))
-        print("num: ", np.binary_repr(instrn, width=8))
+        # print("num: ", np.binary_repr(instrn, width=8))
         if instrn == 0:
             self._done = True
-            return
-        self.INSTRUCTIONS[instrn](num)     
-        if verbose == 1:
+            return     
+        if self._verbose == 1:
             print("pc: ", self._pc)
             print("ir: ", self._ir)
             print("ac: ", self._ac)
+            print("zf: ", self._zero_flag)
+            print("pf: ", self._positive_flag)
+            print()
+        self.INSTRUCTIONS[instrn](num)
         if (jump==False):
             self._pc += 1
 
     def _exec_jump(self, addr):
         self._pc = addr 
-        self.step(1, True)
+        self.step(True)
 
     def _exec_jzer(self, addr):
         if self._zero_flag:
             self._pc = addr     
+            self.step(True)
 
     def _exec_jpos(self, addr):
         if self._positive_flag:
             self._pc = addr 
+            self.step(True)
 
     def _exec_load(self, addr):
         if addr==30:
             self._io += "? "
             user_input = input("? ")
-            self._io += user_input
+            self._io += user_input + "\n"
             self._memory[addr] = np.int8(user_input)
         self._ac = self._memory[addr]
+        self._set_flags()
 
     def _exec_stor(self, addr):
         if addr==31:
             self._io += str(self._ac)
+            self._io += "\n"
         self._memory[addr] = self._ac 
 
     def _exec_sub(self, addr):
         self._ac -= self._memory[addr]
+        self._set_flags()
 
     def _exec_add(self, addr):
         self._ac += self._memory[addr]
+        self._set_flags()
+
+    def _set_flags(self):
+        if self._ac == 0:
+            self._zero_flag = True
+            self._positive_flag = False
+        elif self._ac > 0:
+            self._zero_flag = False
+            self._positive_flag = True
+        else:
+            self._zero_flag = False
+            self._positive_flag = False
+        
 
     def out(self):
         print(self._io)
--- simulator/run.py
+++ simulator/run.py
@@ -5,7 +5,8 @@ mem = np.array([0]*32, dtype=np.int8)
 mem[0] = 33
 mem[1] = -118
 mem[2] = -65
+mem[3] = -98
+mem[4] = -65
 mem[10] = 36
 a = Simulator(mem)
-print("bsdfhjahkb: ", type(mem[1]), mem[1]>>5)
 a.run(1)
